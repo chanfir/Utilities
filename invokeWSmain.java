a3a2c3545c7bcdeab4fb5c0dc0aaa70d02d9eb98
chanfir <35614661+chanfir@users.noreply.github.com>
2020-04-15 02:28:25 +0200

    Add files via upload

---
+++ invokeWSmain.java
@@ -0,0 +1,13 @@
+package test;
+
+public class invokeWSmain {
+
+	public static void main(String[] args) throws Exception {
+		
+		inokeEquityV2ESB obj1 = new inokeEquityV2ESB();
+		obj1.invokeWS();
+		// TODO Auto-generated method stub
+
+	}
+
+}
